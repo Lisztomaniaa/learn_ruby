106aa116b91c5c9b8293d925f702bf32b17f6f37
Klynchyk Liza <lizontik@gmail.com>
2018-07-17 14:46:36 +0300

    Update temperature.rb

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,39 +1,38 @@
 class Temperature
-	attr_accessor :f, :c
+  attr_accessor :f, :c
+  def initialize arry
+    @f = arry[:f]
+    @c = arry[:c]
+  end
 
-	def initialize arry
-		@f = arry[:f]
-		@c = arry[:c]
-	end
+  def in_fahrenheit
+    c * (9.0 / 5) + 32
+  end
 
-	def in_fahrenheit
-			@c * (9.0/5) + 32
-		end
-	end
 
-	def in_celsius
+  def in_celsius
+    (@f - 32) * (5.0/9)
+  end
 
-			(@f - 32) * (5.0/9)
+  def self.from_celsius num
+    new(:c => num)
+  end
 
-	end
-
-	def self.from_celsius num
-		new(:c => num)
-	end
-
-	def self.from_fahrenheit num
-		new(:f => num)
-	end
+  def self.from_fahrenheit num
+    new(:f => num)
+  end
 
 
 class Celsius < Temperature
-	def initialize temp
-		@c = temp
-	end
+  def initialize temp
+    @c = temp
+  end
 end
 
 class Fahrenheit < Temperature
-	def initialize temp
-		@f = temp
-	end
+  def initialize temp
+    @f = temp
+  end
+end
+
 end
